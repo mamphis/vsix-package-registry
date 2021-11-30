c84da66312d767c6530c30f2f831000941ca37b4
mamphis <mattis.wacker@gmx.de>
2021-12-01 00:37:17 +0100

    Server: Create instance of database

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,16 @@
 import { Server } from "./server";
+import { Database } from '../data/database';
+import { SqliteDatabase } from "../data/sqlite/sqlitedatabase";
+
+export let database: Database;
 
 export async function start() {
     const port = parseInt(process.env.PORT ?? '4781');
+    const sqliteDataPath = process.env.SQLITE_DATA_PATH ?? './data.sqlite';
+    database = new SqliteDatabase(sqliteDataPath)
+    await database.init();
 
     const server = new Server(port);
-
     await server.init();
     await server.start();
 }
